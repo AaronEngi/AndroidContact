3777aedf17f0778a44b404f31e031d2341b01cc6
王超 <343331640@qq.com>
2017-03-20 10:39:47 +0800

    操作联系人，新版权限

--- app/src/main/java/tyrael/wang/contact/MainActivity.java
+++ app/src/main/java/tyrael/wang/contact/MainActivity.java
@@ -1,13 +1,18 @@
 package tyrael.wang.contact;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,6 +20,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100;
     Context context = MainActivity.this;
 
     @Override
@@ -30,29 +36,63 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                ContentValues values = new ContentValues();
-                //首先向RawContacts.CONTENT_URI执行一个空值插入，目的是获取系统返回的rawContactId
-                Uri rawContactUri = context.getContentResolver().insert(ContactsContract.RawContacts.CONTENT_URI, values);
-                long rawContactId = ContentUris.parseId(rawContactUri);
+                // Here, thisActivity is the current activity
+                Activity thisActivity = MainActivity.this;
+                if (ContextCompat.checkSelfPermission(thisActivity,
+                        Manifest.permission.READ_CONTACTS)
+                        != PackageManager.PERMISSION_GRANTED) {
 
-                //往data表入姓名数据
-                values.clear();
-                values.put(ContactsContract.Data.RAW_CONTACT_ID, rawContactId);
-                values.put(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
-                values.put(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, "zhangsan");
-                context.getContentResolver().insert(
-                        android.provider.ContactsContract.Data.CONTENT_URI, values);
+                    // Should we show an explanation?
+//                    if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity,
+//                            Manifest.permission.READ_CONTACTS)) {
+//
+//                        // Show an expanation to the user *asynchronously* -- don't block
+//                        // this thread waiting for the user's response! After the user
+//                        // sees the explanation, try again to request the permission.
+//
+//                    } else {
 
-                //往data表入电话数据
-                values.clear();
-                values.put(android.provider.ContactsContract.Contacts.Data.RAW_CONTACT_ID, rawContactId);
-                values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
-                values.put(ContactsContract.CommonDataKinds.Phone.NUMBER, "5554");
-                values.put(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
-                context.getContentResolver().insert(
-                        android.provider.ContactsContract.Data.CONTENT_URI, values);
+                        // No explanation needed, we can request the permission.
+
+                        ActivityCompat.requestPermissions(thisActivity,
+                                new String[]{Manifest.permission.READ_CONTACTS},
+                                MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+//                    }
+                }else{
+                    insertContact();
+                }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    insertContact();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
@@ -75,5 +115,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void insertContact(){
+        ContentValues values = new ContentValues();
+        //首先向RawContacts.CONTENT_URI执行一个空值插入，目的是获取系统返回的rawContactId
+        Uri rawContactUri = context.getContentResolver().insert(ContactsContract.RawContacts.CONTENT_URI, values);
+        long rawContactId = ContentUris.parseId(rawContactUri);
+
+        //往data表入姓名数据
+        values.clear();
+        values.put(ContactsContract.Data.RAW_CONTACT_ID, rawContactId);
+        values.put(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
+        values.put(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, "zhangsan");
+        context.getContentResolver().insert(
+                android.provider.ContactsContract.Data.CONTENT_URI, values);
+
+        //往data表入电话数据
+        values.clear();
+        values.put(android.provider.ContactsContract.Contacts.Data.RAW_CONTACT_ID, rawContactId);
+        values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
+        values.put(ContactsContract.CommonDataKinds.Phone.NUMBER, "5554");
+        values.put(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
+        context.getContentResolver().insert(
+                android.provider.ContactsContract.Data.CONTENT_URI, values);
     }
 }
